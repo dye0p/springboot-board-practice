ee4282994e25410731adb21e0dc2264e773310af
DongYeop <s01054830455@gmail.com>
2024-06-23 19:34:04 +0900

    feat: 예외처리 클래스 NullPointer 발생 검사 로직 추가
    - 메서드 제네릭 타입 명시화
    - fieldError null 검사

--- src/main/java/com/springboot/board/springboot_board/global/exception/ExceptionManager.java
+++ src/main/java/com/springboot/board/springboot_board/global/exception/ExceptionManager.java
@@ -2,6 +2,7 @@ package com.springboot.board.springboot_board.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionManager {
 
-    // MethodArgumentNotValidException 예외를 처리하는 메서드
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        // 유효성 검사 에러 메시지를 ErrorResponse 객체에 담아 반환
-        String errorMessage = e.getBindingResult().getFieldError().getDefaultMessage();
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        FieldError fieldError = e.getBindingResult().getFieldError();
+
+        String errorMessage = (fieldError != null) ? fieldError.getDefaultMessage() : "Validation error occurred";
+
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), errorMessage);
 
         return ResponseEntity
