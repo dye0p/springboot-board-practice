df0ee4f41ea473523ed80ca7c8d3894658eda650
DongYeop <s01054830455@gmail.com>
2024-07-03 09:40:29 +0900

    feat: 이메일 인증 비동기 처리

--- src/main/java/com/springboot/board/springboot_board/domain/member/service/MailService.java
+++ src/main/java/com/springboot/board/springboot_board/domain/member/service/MailService.java
@@ -5,6 +5,7 @@ import com.springboot.board.springboot_board.domain.member.util.CreateMailForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ public class MailService {
     private final JavaMailSender mailSender;
     private final CreateMailForm createMailForm;
 
+    @Async("mailExecutor")
     @Transactional
     public void sendMail(MailSendRequest mailSendRequest) {
         SimpleMailMessage mailMessage = createMailForm.createMailMessaget(mailSendRequest.email());
