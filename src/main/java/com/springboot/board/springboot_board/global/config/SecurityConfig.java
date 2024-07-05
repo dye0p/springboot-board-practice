9e2b1a44f2452241c41f490a897c8eb5c34d3fff
DongYeop <s01054830455@gmail.com>
2024-07-05 21:38:50 +0900

    feat: jwt 환경 설정

--- src/main/java/com/springboot/board/springboot_board/global/config/SecurityConfig.java
+++ src/main/java/com/springboot/board/springboot_board/global/config/SecurityConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,8 +17,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/h2-console/**");
     }
 
     @Bean
@@ -25,18 +28,24 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //무상태 설정
 
         http
                 .authorizeHttpRequests((auth) ->
                         auth.requestMatchers(
-                                        "/", "/h2-console/**",
-                                        "/api/v1/join", "/api/v1/login",
+                                        "/",
+                                        "/api/v1/join",
                                         "/api/v1/check-email", "/api/v1/check-loginid",
                                         "/api/v2/auth/auth-code"
                                 ).permitAll()
                                 .anyRequest().authenticated());
-
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
