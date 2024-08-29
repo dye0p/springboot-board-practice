d872d14d373687e8e688331f153816cf5cd12736
DongYeop <s01054830455@gmail.com>
2024-08-29 18:14:37 +0900

    test: AuthController email 관련 Api Test 완료

--- src/test/java/com/springboot/board/springboot_board/ControllerTestSupport.java
+++ src/test/java/com/springboot/board/springboot_board/ControllerTestSupport.java
@@ -1,14 +1,18 @@
 package com.springboot.board.springboot_board;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.board.springboot_board.application.auth.AuthService;
+import com.springboot.board.springboot_board.application.jwt.TokenService;
 import com.springboot.board.springboot_board.application.member.MemberService;
+import com.springboot.board.springboot_board.presentation.auth.AuthController;
 import com.springboot.board.springboot_board.presentation.member.MemberController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = MemberController.class)
+@WebMvcTest(controllers = {MemberController.class,
+        AuthController.class})
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -19,4 +23,10 @@ public abstract class ControllerTestSupport {
 
     @MockBean
     protected MemberService memberService;
+
+    @MockBean
+    protected AuthService authService;
+
+    @MockBean
+    protected TokenService tokenService;
 }
---
+++ src/test/java/com/springboot/board/springboot_board/presentation/auth/AuthControllerTest.java
@@ -0,0 +1,63 @@
+package com.springboot.board.springboot_board.presentation.auth;
+
+import com.springboot.board.springboot_board.ControllerTestSupport;
+import com.springboot.board.springboot_board.application.auth.dto.MailSendRequest;
+import com.springboot.board.springboot_board.application.auth.dto.MailVerifyRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WithMockUser("user1")
+class AuthControllerTest extends ControllerTestSupport {
+
+    @DisplayName("인증코드를 전송한다.")
+    @Test
+    void sendEmail() throws Exception {
+        //given
+        MailSendRequest mailSendRequest = new MailSendRequest("test@test.com");
+
+        //when //then
+        mockMvc.perform(
+                        post("/api/v2/auth/auth-code")
+                                .content(objectMapper.writeValueAsBytes(mailSendRequest))
+                                .contentType(APPLICATION_JSON)
+                                .with(csrf())
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value("true"))
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("인증코드가 전송되었습니다"));
+
+    }
+
+    @DisplayName("인증코드를 검증한다.")
+    @Test
+    void verifyMailAuthCode() throws Exception {
+        //given
+        MailVerifyRequest mailVerifyRequest = new MailVerifyRequest("12345678", "test@test.com");
+
+        //when //then
+        mockMvc.perform(
+                        get("/api/v2/auth/auth-code")
+                                .content(objectMapper.writeValueAsBytes(mailVerifyRequest))
+                                .contentType(APPLICATION_JSON)
+                                .with(csrf())
+
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value("true"))
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("인증되었습니다"));
+    }
+}
