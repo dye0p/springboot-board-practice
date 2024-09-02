6795d3ec3cf3f9d8b6b801d767e7e66f3742c524
DongYeop <s01054830455@gmail.com>
2024-09-02 17:23:17 +0900

    rename: TokenManager -> TokenCreator 클래스 이름 변경

--- src/main/java/com/springboot/board/springboot_board/application/jwt/TokenCreator.java
+++ src/main/java/com/springboot/board/springboot_board/application/jwt/TokenCreator.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class TokenManager {
+public class TokenCreator {
 
     private final TokenProvider tokenProvider;
 
